b289c12b2a6dbc07183387266ddfebc07a6a8730
Alex Cattell <abcattell91@gmail.com>
2022-09-27 18:04:40 +0100

    setup flat page for a single flat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import FlatList from './FlatList'
+import FlatList from './FlatList';
+import Flat from './Flat';
 
 
 class App extends Component {
   render () {
     return (
       <div>
-        <FlatList />
+        <div><FlatList /></div>
+        <div><Flat /></div>
       </div>
     )
   }
--- src/components/Flat.jsx
+++ src/components/Flat.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Flat = () => {
-  return (
-    <div class="card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg';);">
-      <div class="card-category">150 EUR</div>
-        <div class="card-description">
-          <h2>Super 60m2 in trendy neighborhood!</h2>
-        </div>
-        <a class="card-link" href="#"></a>
-    </div>
-  );
+class Flat extends Component {
+	render () {
+    return (
+      <div className="card">
+        <div className="card-category">{}</div>
+          <div className="card-description">
+          {}
+          </div>
+      </div>
+    )
+  }
 };
 
 export default Flat;
