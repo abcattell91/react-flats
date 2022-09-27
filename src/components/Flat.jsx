5386a945b747855c862b96569aa391ce96156f39
Alex Cattell <abcattell91@gmail.com>
2022-09-27 19:15:49 +0100

    working on rendering images as background inside cards. Currently image urls are displayed inside console but not rendering inside the browser on cards

--- src/components/Flat.jsx
+++ src/components/Flat.jsx
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 
 class Flat extends Component {
 	render () {
+    const img = `${this.props.imageUrl}`
     return (
-      <div className="card">
-        <div className="card-category">{}</div>
+      <div className="card" style={{backgroundImage: `url('${img}')`}}>
+        <div className="card-category">{this.props.price} {this.props.priceCurrency}</div>
           <div className="card-description">
-          {}
+            <h2>{this.props.name}</h2>
           </div>
+          <a className="card-link" href="#"></a>
       </div>
     )
   }
-};
+}
+
+// const Flat = () => {
+//   return (
+//     <div className="card">
+//       <div className="card-category">150 EUR</div>
+//         <div className="card-description">
+//           <h2>Super 60m2 in trendy neighborhood!</h2>
+//         </div>
+//         <a className="card-link" href="#"></a>
+//     </div>
+//   );
+// };
 
 export default Flat;
--- src/components/FlatList.jsx
+++ src/components/FlatList.jsx
@@ -1,13 +1,18 @@
-import react, { component } from 'react';
+import React, { component } from 'react';
 import flats from '../../data/Flats';
 import Flat from './Flat';
 
 const FlatList = () => {
   return (
-    flats.map((element) => {
-      console.log(element.imageUrl)
-    })
-  );
-}
-
+    flats.map(({ lat, lng, name, imageUrl, price, priceCurrency }) =>
+    <Flat
+      lat={lat}
+      lng={lng}
+      name={name}
+      imageUrl={imageUrl}
+      price={price}
+      priceCurrency={priceCurrency}
+      key={lat}
+    />)
+)};
 export default FlatList;
