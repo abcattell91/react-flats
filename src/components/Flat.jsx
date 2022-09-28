fc22f9eba1eb1b44ac81055878301674696f40f6
Alex Cattell <abcattell91@gmail.com>
2022-09-28 15:51:38 +0100

    added images to cards

--- src/components/Flat.jsx
+++ src/components/Flat.jsx
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 
 class Flat extends Component {
 	render () {
-    const img = `${this.props.imageUrl}`
+    const style = {
+      backgroundImage: `linear-gradient(
+        rgba(0, 0, 0, 0.3),
+        rgba(0, 0, 0, 0.2)),
+        url(${this.props.imageUrl})`,
+      width: "350px",
+      height: "200px",
+      backgroundSize: "cover"
+  }
     return (
-      <div className="card" style={{backgroundImage: `url('${img}')`}}>
+      <div className="card" style={style}>
         <div className="card-category">{this.props.price} {this.props.priceCurrency}</div>
-          <div className="card-description">
+          <div className="card-description" >
             <h2>{this.props.name}</h2>
           </div>
           <a className="card-link" href="#"></a>
